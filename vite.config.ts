5180397e144cf0c052e90218461b5e14fe4461f1
snacksncode <snacksncode@gmail.com>
2023-03-31 07:27:35 +0200

    cache images

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "*.png", "*.svg"],
       manifest: {
         name: "Geolocation PWA",
         short_name: "GeoPWA",
