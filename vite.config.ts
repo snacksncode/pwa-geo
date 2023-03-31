51954a2a73b77b9b41614ff4bcac720d2e92c970
snacksncode <snacksncode@gmail.com>
2023-03-31 07:48:19 +0200

    cache map?

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,39 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
+const getCache = ({ name, pattern }: any) => ({
+  urlPattern: pattern,
+  handler: "CacheFirst" as const,
+  options: {
+    cacheName: name,
+    expiration: {
+      maxEntries: 500,
+      maxAgeSeconds: 60 * 60 * 24 * 365 * 2, // 2 years
+    },
+    cacheableResponse: {
+      statuses: [200],
+    },
+  },
+});
+
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
       registerType: "autoUpdate",
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "*.png", "*.svg"],
+      workbox: {
+        runtimeCaching: [
+          getCache({
+            pattern: /.*openstreetmap.*/,
+            name: "map-tiles",
+          }),
+          getCache({
+            pattern: /.*leaflet.*/,
+            name: "map-assets",
+          }),
+        ],
+      },
       manifest: {
         name: "Geolocation PWA",
         short_name: "GeoPWA",
